616756d1699a80d56326835a3801ccfaa627c38a
Ish <ishmell.ramirez@gmail.com>
2021-02-21 20:28:50 -0500

    finished problem

---
+++ quiz.js
@@ -0,0 +1,8 @@
+const PI = 3.14;
+
+/*
+var is global and can be redeclared where let cannot.
+const cannot be redeclared or reassigned and is not global.
+let can be reassigned but const cannot.
+hoisting is when javascript declares a var before assigning its value.
+*/
